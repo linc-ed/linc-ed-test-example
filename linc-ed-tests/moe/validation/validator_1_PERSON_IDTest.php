549fe38457f9018f7817d9648829fd5a7ff941ed
linc-ed <paul@linc-ed.com>
2014-10-09 12:36:55 +1300

    Tuition field test added

--- linc-ed-tests/moe/validation/validator_1_PERSON_IDTest.php
+++ linc-ed-tests/moe/validation/validator_1_PERSON_IDTest.php
@@ -392,6 +392,16 @@ class validator_1_PERSON_Test extends PHPUnit_Framework_TestCase {
 		$this->assertSame($valid, 'true');
 
 	}
+
+
+	public function testTuitionField(){
+
+		$moe = new MOEValidator(MOECodeSets::$students[353], 'M', MOECodeSets::$schools[1234]);
+		$valid = $moe->check_22();
+		$this->assertSame($valid, 'true');
+
+	}
+
 }
 
 ?>
