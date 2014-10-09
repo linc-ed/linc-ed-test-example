c24728294b53992769a5db8efde724f42d112ed3
Lewis Christie <lewis.christie@gmail.com>
2014-10-09 17:39:22 +0200

    add FIRSTNAME test case

---
+++ linc-ed-tests/moe/validation/validator_5_FIRSTNAMETest.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Field name: FIRSTNAME
+ * Field number: 5
+ * ASCII or ASCII plus macronised, mandatory
+ * field should not contain brackets, commas or multiple spaces before or after hyphens or apostrophes.
+ */
+error_reporting(E_ALL);
+date_default_timezone_set('Pacific/Auckland');
+require_once(dirname(__FILE__).'/../../../moe/MOEValidateUpdated.php');
+require_once(dirname(__FILE__).'/../../../moe/moe_test.php');
+require_once(dirname(__FILE__).'/StudentData.php');
+require_once(dirname(__FILE__).'/SchoolData.php');
+
+class validator_5_FIRSTNAMETest extends PHPUnit_Framework_TestCase {
+
+	private $student;
+	private $school;
+
+	//Reset the student and school data before each test method
+	public function setUp() {
+		$this->student = StudentData::getStudent();
+		$this->school = SchoolData::getSchool();
+	}
+
+	public function testValidSurname() {
+		$moe = new MOEValidator($this->student, 'M', $this->school);
+		$valid = $moe->check_3();
+		$this->assertSame($valid, 'true');
+	}
+
+	public function testSurnameIsMandatory() {
+		$this->student['first_name'] = '';
+		$moe = new MOEValidator($this->student, 'M', $this->school);
+		$valid = $moe->check_3();
+		$this->assertSame($valid, 'false');
+	}
+
+	public function testSurnameDoesNotContainBrackets() {
+		$this->student['first_name'] = 'a(b)';
+		$moe = new MOEValidator($this->student, 'M', $this->school);
+		$valid = $moe->check_3();
+		$this->assertSame($valid, 'false');
+	}
+
+	public function testSurnameDoesNotContainCommas() {
+		$this->student['first_name'] = 'a,b';
+		$moe = new MOEValidator($this->student, 'M', $this->school);
+		$valid = $moe->check_3();
+		$this->assertSame($valid, 'false');
+	}
+
+	public function testSurnameMultipleSpacesWithHyphen() {
+		//one space
+		$this->student['first_name'] = 'a - b';
+		$moe = new MOEValidator($this->student, 'M', $this->school);
+		$valid = $moe->check_3();
+		$this->assertSame($valid, 'true');
+
+		//two space before
+		$this->student['first_name'] = 'a  - b';
+		$moe = new MOEValidator($this->student, 'M', $this->school);
+		$valid = $moe->check_3();
+		$this->assertSame($valid, 'false');
+
+		//two space after
+		$this->student['first_name'] = 'a -  b';
+		$moe = new MOEValidator($this->student, 'M', $this->school);
+		$valid = $moe->check_3();
+		$this->assertSame($valid, 'false');
+	}
+
+	public function testSurnameMultipleSpacesWithApostrophie() {
+		//one space
+		$this->student['first_name'] = 'a \' b';
+		$moe = new MOEValidator($this->student, 'M', $this->school);
+		$valid = $moe->check_3();
+		$this->assertSame($valid, 'true');
+
+		//two space before
+		$this->student['first_name'] = 'a  \' b';
+		$moe = new MOEValidator($this->student, 'M', $this->school);
+		$valid = $moe->check_3();
+		$this->assertSame($valid, 'false');
+
+		//two space after
+		$this->student['first_name'] = 'a \'  b';
+		$moe = new MOEValidator($this->student, 'M', $this->school);
+		$valid = $moe->check_3();
+		$this->assertSame($valid, 'false');
+	}
+}
